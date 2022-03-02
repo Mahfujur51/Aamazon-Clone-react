55219bf7f880499342a84b4a677ecd67f6795e67
Mahfujur Rahman Moon <37837514+Mahfujur51@users.noreply.github.com>
2022-03-02 18:07:41 +0600

    product pass dynamic

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
                 <img className='home_image' src="https://m.media-amazon.com/images/I/61DUO0NqyyL._SX3740_.jpg" alt="" />
                 <div className='home_row'>
                     <Product title="The  Lean startup" price={29.99} image="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_Returns_1x._SY304_CB432774714_.jpg" rating={5} />
-                    {/* <Product /> */}
+                    {/* <Product title="" /> */}
 
                     {/* Product  */}
                 </div>
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -6,13 +6,15 @@ const Product = ({title,price,rating,image}) => {
   return (
     <div className='product'>
         <div className="product_info">
-            <p>Lorem ipsum</p>
+            <p>{title}</p>
             <p className='product_price'>
                 <small>$</small>
-                <strong>11.96</strong>
+                <strong>{price}</strong>
             </p>
             <div className='product_rating'>
-                 <p>⭐</p>
+                {Array(rating).fill().map((_,i)=>(
+                        <p key={i}>⭐</p>
+                ))}
             </div>
         </div>
         <img src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_Returns_1x._SY304_CB432774714_.jpg" alt="" />
